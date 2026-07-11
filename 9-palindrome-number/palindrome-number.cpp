5730d761b280f41f324bb723bab8bc2bbd2208b9
Shivendrak Maurya <167509510+Shivendrakmaurya@users.noreply.github.com>
2026-07-11 20:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.09%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))
-            return false;
-
+        if (x < 0 || (x % 10 == 0 && x != 0)){
+             return false;
+        }
+           
         int rev = 0;
 
         while (x > rev) {
