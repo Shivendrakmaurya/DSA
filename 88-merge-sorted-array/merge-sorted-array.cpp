781b562d04907ad31ebbfd1b45ec92b4121ccb98
Shivendrak Maurya <167509510+Shivendrakmaurya@users.noreply.github.com>
2026-07-12 23:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.54%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
         int idx = m+n-1, i = m-1, j = n-1;
 
         while(i>=0 && j>=0 ){
